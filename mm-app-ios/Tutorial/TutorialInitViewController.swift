0bbdeba71a53dfc0db31d16285dcf0c75678efb0
Valmir <valmir@megaleios.com>
2019-07-31 17:59:59 -0300

    Creating Tutorial page

---
+++ mm-app-ios/Tutorial/TutorialInitViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TutorialInitViewController.swift
+//  mm-app-ios
+//
+//  Created by Megaleios DEV on 31/07/19.
+//  Copyright © 2019 Megaleios DEV. All rights reserved.
+//
+
+import UIKit
+
+class TutorialInitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+
+}
